0b1e2398026eb660eb9b5e3bb4ced9b01f2faaec
Ferdy Hahan Pradana <ferdyhahan5@gmail.com>
2025-05-03 03:23:39 +0700

    fix namespace error

--- src/ExcelPlatMerahStyle.php
+++ src/ExcelPlatMerahStyle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\ExcelPlatMerah;
+namespace Ferdyhape\ExcelPlatMerah;
 
 use PhpOffice\PhpSpreadsheet\Style\Alignment as StyleAlignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
